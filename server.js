4416d8f68f978fd3817282d4868d83fa008f6e7e
Luis Gonzaga d'Orey Roquette Travassos Valdez <luisrvaldez@gmail.com>
2021-03-08 13:52:29 +0000

    asd
    asddsa

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const http = require('http');
+//const hostname = 'luisrvaldez-testapp.herokuapp.com';
+const hostname = 'localhost';
+const port = 8815;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
+//server.on('listenining', function () {
+//  console.log('ok, server is running');
+//});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+//server.listen(port);
